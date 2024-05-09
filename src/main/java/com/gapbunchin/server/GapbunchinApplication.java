f9562c5697098307c89643c813911efe1d1efbca
s333o <ramgthunder12@gmail.com>
2024-05-09 15:30:28 +0900

    new server

---
+++ src/main/java/com/gapbunchin/server/GapbunchinApplication.java
@@ -0,0 +1,13 @@
+package com.gapbunchin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GapbunchinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GapbunchinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gapbunchin/server/GapbunchinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gapbunchin.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GapbunchinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
