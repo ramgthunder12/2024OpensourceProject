a4eb50926bbcb4fbdae4ffdd9955a55f71932ac1
meadak <151588821+meadak@users.noreply.github.com>
2024-06-07 20:06:31 +0900

    Update ChatController.java

--- src/main/java/com/gapbunchin/server/controller/ChatController.java
+++ src/main/java/com/gapbunchin/server/controller/ChatController.java
@@ -1,1 +1,18 @@
+package com.gapbunchin.server.controller;
+
+import com.gapbunchin.server.entity.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/message")
+    @SendTo("/topic/messages")
+    public Message send(Message message) {
+        return new Message(HtmlUtils.htmlEscape(message.getAuthor()), HtmlUtils.htmlEscape(message.getMessage()));
+    }
+}
 
